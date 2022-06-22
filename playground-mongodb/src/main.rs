5037bf754cc212cc6e904b283552dd22c545163a
Manuel Hutter <manuel@hutter.io>
2022-06-22 21:24:47 +0200

    refactor(mongodb): Shorten, document
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- playground-mongodb/src/main.rs
+++ playground-mongodb/src/main.rs
@@ -1,6 +1,6 @@
 use futures::TryStreamExt;
 use mongodb::{
-    bson::{doc, from_bson, Bson, Document, RawDocumentBuf},
+    bson::{self, doc, Document, RawDocumentBuf},
     options::ClientOptions,
     Client, Database,
 };
@@ -11,9 +11,7 @@ async fn main() -> mongodb::error::Result<()> {
     let client = connect().await?;
     let db = client.default_database().unwrap();
 
-    so72696316(db).await?;
-
-    Ok(())
+    so72696316(db).await
 }
 
 async fn connect() -> mongodb::error::Result<Client> {
@@ -22,18 +20,24 @@ async fn connect() -> mongodb::error::Result<Client> {
             ClientOptions::parse("mongodb://localhost:27017/local?appName=rust-playground-mongodb")
                 .await?;
         options.app_name = Some(env!("CARGO_CRATE_NAME").to_string());
+
+        // for more options see:
+        // https://docs.rs/mongodb/latest/mongodb/options/struct.ClientOptions.html
+
         Client::with_options(options)?
     };
 
+    // Ping the DB to confirm we have a connection
     client
         .database("admin")
-        .run_command(doc! {"ping":1}, None)
+        .run_command(doc! {"ping": 1}, None)
         .await?;
-    println!("Connection successful!");
+    println!("---> Connection successful!");
 
     Ok(client)
 }
 
+/// A sample struct to use in our program
 #[derive(Debug, Serialize, Deserialize)]
 struct Book {
     author: String,
@@ -41,18 +45,17 @@ struct Book {
 }
 
 async fn so72696316(db: Database) -> mongodb::error::Result<()> {
-    seed_so72696316(db.clone()).await?;
     use rayon::prelude::*;
-
+    seed_so72696316(db.clone()).await?;
     let coll = db.collection("books");
+
+    // Fetch all documents but do not deserialize them yet
     let docs: Vec<RawDocumentBuf> = coll.find(None, None).await?.try_collect().await?;
 
+    // Deserialize all documents using Rayon's parallel iterator
     let z: Vec<Book> = docs
         .par_iter()
-        .map(|x| {
-            let doc = x.to_document().unwrap();
-            from_bson(Bson::Document(doc))
-        })
+        .map(|raw| bson::from_slice(raw.as_bytes()).unwrap())
         .collect();
 
     dbg!(&z);
@@ -61,6 +64,7 @@ async fn so72696316(db: Database) -> mongodb::error::Result<()> {
     Ok(())
 }
 
+/// Seed the "books" collection with some entries if it is empty.
 async fn seed_so72696316(db: Database) -> mongodb::error::Result<()> {
     let books = db.collection::<Document>("books");
 
